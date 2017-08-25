60ef05c9a7a5cd2f65d7686f6cc85cd408cf2978
fangnavinfo <fangshishuai@navinfo>
2017-08-25 09:50:12 +0800

    init
    init project

---
+++ gitLearn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitLearn
+//
+//  Created by fang on 2017/8/25.
+//  Copyright © 2017年 fang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
